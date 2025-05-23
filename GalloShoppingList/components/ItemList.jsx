8b612781c388724fb74e5112dffddaed866f5fcf
mariadiasss <mariacoelhodias94@gmail.com>
2025-05-23 12:18:26 -0300

    Colocamos botão de item comprado

--- GalloShoppingList/components/ItemList.jsx
+++ GalloShoppingList/components/ItemList.jsx
@@ -5,11 +5,22 @@ import { Ionicons } from '@expo/vector-icons'
 export default function ItemList({ item, markItem, unmarkItem, removeItem }) {
   return (
     <View style={styles.itemList}>
-        <Text style={item?.bought ? styles.itemBought : styles.itemToBuy}>{item?.name}</Text>
-        <TouchableOpacity style={styles.actionIcon}>
-          <Ionicons name ='bag-check-outline' size={24} color='#fff'/>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.actionIcon, { backgroundColor: "darkred"}]}>
+        <Text style={item?.bought ? styles.itemBought : styles.itemToBuy}>
+          {item?.name}
+        </Text>
+        {!item?.bought ? (
+          <TouchableOpacity style={styles.actionIcon} onPress={() => markItem(item.id)}>
+            <Ionicons name ='bag-check-outline' size={24} color='#fff'/>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={styles.actionIcon} onPress={() => unmarkItem(item.id)}>
+            <Ionicons name ='bag-remove-outline' size={24} color='#fff'/>
+          </TouchableOpacity>
+        )} 
+        <TouchableOpacity 
+        style={[styles.actionIcon, { backgroundColor: "darkred"}]}
+        onPress={() => removeItem(item.id)}
+        >
           <Ionicons name = 'trash-bin-outline' size ={24} color='#fff'/>
         </TouchableOpacity>
     </View>
@@ -35,6 +46,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textDecorationLine: 'none'
   },
+  itemBought:{
+    flex: 1,
+    color: '#fff',
+    fontSize: 24,
+    textDecorationLine: 'line-through'
+},
   actionIcon:{
     height: 40,
     width: 40,
